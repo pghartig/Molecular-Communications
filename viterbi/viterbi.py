c61122e9045b2bee86dbab1c889826797bf633a0
pgregory <pdlaxin@gmail.com>
2019-10-16 10:04:29 +0200

    working on viterbi setup

--- viterbi/viterbi.py
+++ viterbi/viterbi.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 
-def viterbi_output(input_data,CSI):
-    decisions = np.zeros(CSI.shape)
-    return decisions
+def viterbi_output(metrics):
+    survivor_paths = np.zeros((metrics.shape[0]/2,metrics.shape[1]), dtype=np.int8)
+
+    final_path_ind = np.argmin(np.sum(survivor_paths,axis=1))
+    return np.flip(survivor_paths[final_path_ind, :],axis=1)
