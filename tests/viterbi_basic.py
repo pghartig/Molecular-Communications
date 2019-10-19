38f0f3cbef2dbf10bbf6bb48d0be148ca83b74da
pgregory <pdlaxin@gmail.com>
2019-10-19 19:48:50 +0200

    some updates to viterbi work

--- tests/viterbi_basic.py
+++ tests/viterbi_basic.py
@@ -1,14 +1,17 @@
 from communication_util.data_gen import *
+from viterbi.viterbi import *
 
 
 def test_viterbi_gaussian():
     #setup data
     #TODO make consolidate this part
     data_gen = training_data_generator(plot=True)
+    data_gen.random_bit_stream()
     data_gen.setup_channel(shape=None)
     data_gen.random_bit_stream()
     data_gen.send_through_channel()
 
     #detect with Viterbi and check WER
+    viterbi_output(transmit_alphabet, data_gen.channel_output, data_gen.CIR_matrix)
 
     assert False
