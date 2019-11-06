25ad17b53640cf6215c15bb0e325924ad33cfebe
pgregory <pdlaxin@gmail.com>
2019-11-06 15:28:33 +0100

    viterbi testing correctly

--- tests/Viterbi/viterbi_reimplemented.py
+++ tests/Viterbi/viterbi_reimplemented.py
@@ -5,12 +5,13 @@ from communication_util.error_rates import *
 def test_viterbi_new_gaussian():
     error_tolerance = np.power(10.0, -3)
     # setup data
-    channel = np.zeros((1, 8))
+    channel = np.zeros((1, 5))
     # channel[0, [0, 3, 4, 5]] = 1, 0.5, 0.1, 0.2
-    channel[0, [0]] = 1
+    channel[0, [0, 3]] = 1, 0.7
+    # channel[0, [0]] = 1
 
     # TODO make consolidate this part
-    data_gen = training_data_generator(SNR=5, channel=channel, plot=True)
+    data_gen = training_data_generator(SNR=1, channel=channel, plot=True)
     # data_gen = training_data_generator(plot=True)
 
     data_gen.setup_channel(shape=None)
@@ -22,6 +23,5 @@ def test_viterbi_new_gaussian():
     metric = gaussian_channel_metric_working(channel, data_gen.channel_output)
     detected = viterbi_setup_with_nodes(data_gen.alphabet, data_gen.channel_output, data_gen.CIR_matrix.shape[1],
                                         metric.metric)
-    # check WER
     ser = symbol_error_rate(detected, data_gen.symbol_stream_matrix)
     assert error_tolerance >= ser
